8fb6e541dc4004f8e9616ccf124151eb67159bd7
raulc <raul.cardoso@outlook.com.br>
2023-03-28 12:55:05 -0300

    Implementado model de pontos turísticos; Adicionadas algumas dependências;

---
+++ lib/model/ponto_turistico.dart
@@ -0,0 +1,31 @@
+
+import 'package:intl/intl.dart';
+
+class PontoTutistico {
+
+  static const CAMPO_ID = 'id';
+  static const CAMPO_NOME = 'nome';
+  static const CAMPO_PAIS = 'pais';
+  static const CAMPO_CIDADE = 'cidade';
+  static const CAMPO_DESCRICAO = 'descricao';
+  static const CAMPO_DATA_INC = 'data';
+
+  int id;
+  String nome;
+  String descricao;
+  DateTime data_inclusao;
+
+  PontoTutistico({
+      required this.id,
+      required this.nome,
+      required this.descricao,
+      required this.data_inclusao
+  });
+
+  String get retornarDataInclusaoFormatada {
+    if (this.data_inclusao == null) {
+      return "";
+    }
+    return DateFormat('dd/MM/yyyy').format(this.data_inclusao!);
+  }
+}
