5c616bdd895315cf7cf1d9a6570eb1dc6a981f57
raulc <raul.cardoso@outlook.com.br>
2023-04-11 20:21:55 -0300

    Adicionada dependência sqflite; Criado o fonte provider.dart que irá gerenciar o banco de dados;

---
+++ lib/database/provider.dart
@@ -0,0 +1,47 @@
+
+import 'package:projeto/model/ponto_turistico.dart';
+import 'package:sqflite/sqflite.dart';
+
+class Provider {
+  static const _dbName = 'pontos-turisticos.db';
+  static const _dbVersion = 1;
+
+  Provider._init();
+  static final Provider instance = Provider._init();
+
+  Database? _database;
+
+  Future<Database> get database async => _database ??=await _initDatabase();
+
+  Future<Database> _initDatabase() async {
+    final databasesPath = await getDatabasesPath();
+    final dbPath = '$databasesPath/$_dbName';
+
+    return await openDatabase(
+      dbPath,
+      version: _dbVersion,
+      onCreate: _onCreate,
+      //onUpgrade: _onUpgrade,
+    );
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute(
+        '''
+          CREATE TABLE pontoTuristico (
+            ${PontoTutistico.CAMPO_ID} INTEGER PRIMARY KEY AUTOINCREMENT,
+            ${PontoTutistico.CAMPO_NOME} TEXT NOT NULL,
+            ${PontoTutistico.CAMPO_DESCRICAO} TEXT,
+            ${PontoTutistico.CAMPO_CIDADE} TEXT NOT NULL,
+            ${PontoTutistico.CAMPO_PAIS} TEXT NOT NULL,
+          );  
+        '''
+    );
+  }
+
+  Future<void> close() async {
+    if (_database != null) {
+      await _database!.close();
+    }
+  }
+}
