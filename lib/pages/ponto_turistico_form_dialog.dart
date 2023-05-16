32c30d41b8fc203d8d14ac2d86c84f4fc138c757
raulc <raul.cardoso@outlook.com.br>
2023-05-16 20:31:50 -0300

    Implementado para abrir o mapa pelo Google Maps de acordo com a localização do dispositivo

--- lib/pages/ponto_turistico_form_dialog.dart
+++ lib/pages/ponto_turistico_form_dialog.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:intl/intl.dart';
+import 'package:maps_launcher/maps_launcher.dart';
 import '../model/ponto_turistico.dart';
 
 class PontoTuristicoDialog extends StatefulWidget{
@@ -20,6 +22,8 @@ class PontoTuristicoAtualState extends State<PontoTuristicoDialog>{
   final descricaoController = TextEditingController();
   final dtInclusaoController = TextEditingController();
   final _dateFormat = DateFormat('dd/MM/yyy');
+
+  Position? _localizacaoAtual;
 
   @override
   void initState(){
@@ -80,6 +84,83 @@ class PontoTuristicoAtualState extends State<PontoTuristicoDialog>{
               return null;
             },
           ),
+          ElevatedButton(
+              onPressed: _abrirMapaExterno,
+              child: Text('Abrir local no Google Maps')
+          ),
+          ElevatedButton(
+              onPressed: () {},
+              child: Text('Abrir local no aplicativo')
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<bool> _obterLocalizacaoAtual() async{
+    bool servicoHabilitado = await _servicoHabilitado();
+    if(!servicoHabilitado){
+      return false;
+    }
+    bool permissoesPermitidas = await _permissoesPermitidas();
+    if(!permissoesPermitidas){
+      return false;
+    }
+    _localizacaoAtual = await Geolocator.getCurrentPosition();
+    return true;
+  }
+
+  void _abrirMapaExterno() async{
+    bool obteveLocalizacaoAtual = await _obterLocalizacaoAtual();
+
+    if (obteveLocalizacaoAtual) {
+      MapsLauncher.launchCoordinates(
+          _localizacaoAtual!.latitude, _localizacaoAtual!.longitude);
+    }
+  }
+
+  Future<bool> _servicoHabilitado() async {
+    bool servicoHabilitado = await Geolocator.isLocationServiceEnabled();
+    if (!servicoHabilitado){
+      await _mostrarDialogMensagem('Para utilizar esse recurso, você deverá habilitar o serviço'
+          ' de localização');
+      Geolocator.openLocationSettings();
+      return false;
+    }
+    return true;
+  }
+  Future<bool> _permissoesPermitidas() async {
+    LocationPermission permissao = await Geolocator.checkPermission();
+    if(permissao == LocationPermission.denied){
+      permissao = await Geolocator.requestPermission();
+      if(permissao == LocationPermission.denied){
+        _mostrarMensagem('Não será possível utilizar o recurso '
+            'por falta de permissão');
+      }
+    }
+    if(permissao == LocationPermission.deniedForever){
+      await _mostrarDialogMensagem('Para utilizar esse recurso, você deverá acessar '
+          'as configurações do app para permitir a utilização do serviço de localização');
+      Geolocator.openAppSettings();
+      return false;
+    }
+    return true;
+  }
+  void _mostrarMensagem(String mensagem){
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(mensagem)));
+  }
+
+  Future<void> _mostrarDialogMensagem(String mensagem) async {
+    await showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text('Atenção'),
+        content: Text(mensagem),
+        actions: [
+          TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('OK')
+          )
         ],
       ),
     );
